057c4e982870b5fe5c437480d937d96ac70170a7
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:34:19 +0700

    Add files via upload

---
+++ Latihanpbo2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Stks
+ */
+public class Latihanpbo2 {
+     public static void main(String[] args) {
+         //deklarasi Variable
+      int nilaiInt;
+      final double PHI = 3.14;  //konstanta uppercase
+      boolean nilaiLogika;
+      char nilaiKarakter;
+      
+      //memberi nilai ke variable
+      nilaiInt= 78;
+      nilaiLogika= false;
+      nilaiKarakter= 'D';
+      
+      //menampilkanhasil
+      System.out.println();
+      System.out.println("Isi variable nilai=" + nilaiInt);
+      System.out.println("Isi variable PHI="+ PHI);
+      System.out.println("Isi variable logika=" + nilaiLogika);
+      System.out.println("Isi variable Karakter=" + nilaiKarakter);
+      
+     }
+    
+}
